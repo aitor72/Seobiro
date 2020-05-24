d2a461933c4bcb0d19981ddbe899963e9d94ba6e
Aitor <soy@aitor.me>
2020-05-24 18:23:05 +0200

    Initial

---
+++ example/example.php
@@ -0,0 +1,5 @@
+<?php
+include __DIR__ . "/../vendor/autoload.php";
+
+$hello = new \aitor\seobiro\Seobiro();
+echo $hello->echoPhrase('Composer');
---
+++ src/Seobiro.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace aitor\seobiro;
+
+class Seobiro
+{
+
+    public function __construct()
+    {
+        // constructor body
+    }
+
+
+    public function echoPhrase(string $phrase): string
+    {
+        return $phrase;
+    }
+}
